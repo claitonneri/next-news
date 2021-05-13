aec877e07b1798454739de83e2143411c17aa402
Claiton Neri <claitondidone@gmail.com>
2021-05-12 22:09:40 -0300

    feat: integração com Stripe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,19 @@ import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news logo" />
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a href="/">
+          <img src="/images/logo.svg" alt="ig.news logo" />
+        </a>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
 
-                <SignInButton />
-            </div>
-        </header>
-    )
+        <SignInButton />
+      </div>
+    </header>
+  )
 }
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
 import { signIn, useSession } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,10 +12,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('google');
+      return;
     }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+
   }
 
   return (
